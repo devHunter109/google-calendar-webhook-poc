2bae4a83514506f544bd4358cc33f50d2f838671
Michal Michalczuk <michal.michalczuk@tektitconsulting.com>
2022-10-24 12:48:14 +0200

    added google auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import "dotenv/config"
+
+import crypto from "node:crypto"
 import fastify from "fastify"
+import { google } from "googleapis"
 import { logger } from "./logger"
 import pino from "pino"
+
+const googleCredentials = {
+  clientId: process.env["GOOGLE_CLIENT_ID"],
+  secret: process.env["GOOGLE_CLIENT_SECRET"],
+}
 
 const server = fastify({
   logger: pino({
@@ -11,6 +20,25 @@ const server = fastify({
     },
   }),
 })
+
+const oAuthState = crypto.randomBytes(32).toString("hex")
+const redirectUri = "http://localhost:3031/callback"
+const oAuth2Client = new google.auth.OAuth2(
+  googleCredentials.clientId,
+  googleCredentials.secret,
+  redirectUri,
+)
+
+const authUrl = oAuth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: "https://www.googleapis.com/auth/calendar.events",
+  redirect_uri: redirectUri,
+  state: oAuthState,
+  client_id: googleCredentials.clientId,
+})
+
+server.get("/", () => `Authorize your application by\n<a href=${authUrl}>${authUrl}</a>`)
+server.get("/callback", (request) => `Redirected from provider. Raw request\n${request.body}`)
 
 server.listen({ port: 3031 }, (error, address) => {
   if (error) {
